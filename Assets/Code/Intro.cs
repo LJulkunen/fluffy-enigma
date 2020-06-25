7009a3bfeb2dbe554fa856a2df51ee5cb34cd57d
Laura <laura.julkunen@tuni.fi>
2020-06-25 14:29:05 +0300

    First dialogue box visible right away

--- Assets/Code/Intro.cs
+++ Assets/Code/Intro.cs
@@ -29,20 +29,17 @@ public class Intro : MonoBehaviour
     }
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (!dialogueBoxObject.activeInHierarchy)
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-                if (!dialogueBoxObject.activeInHierarchy)
-                {
-                    dialogueBoxObject.SetActive(dialogueBoxObject);
-                    dialogueTextObject.SetActive(dialogueTextObject);
-                    dialogueManager.StartDialogue(dialogue);
-                    flashText.SetActive(true);
-                    InvokeRepeating("FlashTheText", 0f, 0.5f);
-                }
-            }
-            else if (dialogueBoxObject.activeInHierarchy && dialogueManager.sentences.Count > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            dialogueBoxObject.SetActive(dialogueBoxObject);
+            dialogueTextObject.SetActive(dialogueTextObject);
+            dialogueManager.StartDialogue(dialogue);
+            flashText.SetActive(true);
+            InvokeRepeating("FlashTheText", 0f, 0.5f);
+        }
+        else if (Input.touchCount > 0)
+        {
+            if (dialogueBoxObject.activeInHierarchy && dialogueManager.sentences.Count > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
             {
                 dialogueManager.DisplayNextSentence();
             } else if (dialogueBoxObject.activeInHierarchy && dialogueManager.sentences.Count == 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
