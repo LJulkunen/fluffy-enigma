1c27d466131df9cc85310d84e77dac7bd0f3eb9d
Toni <toni.sundell@tuni.fi>
2020-07-04 11:57:33 +0300

    Editor script now in Editor folder

--- Assets/Code/SaveSerial.cs
+++ Assets/Code/SaveSerial.cs
@@ -1,38 +1,7 @@
 ﻿using UnityEngine;
 using System;
 using TMPro;
-using UnityEditor;
 using UnityEngine.SceneManagement;
-
-[CustomEditor(typeof(SaveSerial))]
-public class SaveSerialEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-
-        if (SaveSerial.SAVE == null)
-        {
-            Debug.LogError("No SaveSerial instanses! Please reload scene!");
-            return;
-        }
-
-        if (SaveLoad.SaveData == null)
-            SaveSerial.SAVE.LoadGame();
-
-        SaveSerial.SAVE.Start();
-
-        EditorGUILayout.SelectableLabel(
-            "Read only savedata!!! To add new savedata line edit:" + Environment.NewLine + 
-            "Savedata.Line but keep LineCount as last enum element!");
-
-        for (int i = 0; i < SaveLoad.SaveDataSize; i++)
-        {
-            // Normal editor box with name on left and variable on right
-            EditorGUILayout.LongField(((SaveLoad.Line)i).ToString(), SaveLoad.SaveData[i]);
-        }
-    }
-}
 
 [ExecuteInEditMode]
 public class SaveSerial : MonoBehaviour
@@ -408,6 +377,7 @@ public class SaveSerial : MonoBehaviour
 
     private void OnApplicationQuit()
     {
+        if (Application.isEditor) SaveGame();
         Debug.LogError("Exiting");
     }
 }
---
+++ Assets/Editor/Code/SaveSerialEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SaveSerial))]
+public class SaveSerialEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (SaveSerial.SAVE == null)
+        {
+            Debug.LogError("No SaveSerial instanses! Please reload scene!");
+            return;
+        }
+
+        if (SaveLoad.SaveData == null)
+            SaveSerial.SAVE.LoadGame();
+
+        SaveSerial.SAVE.Start();
+
+        EditorGUILayout.SelectableLabel(
+            "Read only savedata!!! To add new savedata line edit:" + Environment.NewLine +
+            "Savedata.Line but keep LineCount as last enum element!");
+
+        for (int i = 0; i < SaveLoad.SaveDataSize; i++)
+        {
+            // Normal editor box with name on left and variable on right
+            EditorGUILayout.LongField(((SaveLoad.Line)i).ToString(), SaveLoad.SaveData[i]);
+        }
+    }
+}
