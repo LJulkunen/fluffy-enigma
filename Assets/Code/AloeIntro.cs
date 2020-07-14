f36d55ea629d225dd32d17be9f91d1d3d120742c
Laura <laura.julkunen@tuni.fi>
2020-07-14 19:15:08 +0300

    Working on intro for the aloe bad end

---
+++ Assets/Code/AloeIntro.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AloeIntro : MonoBehaviour
+{
+    SaveSerial save;
+
+    //public GameObject textPlacementObject;
+
+    public Dialogue dialogue;
+    public DialogueManager dialogueManager;
+    public GameObject dialogueBoxObject;
+    public GameObject dialogueTextObject;
+    public TextMeshProUGUI dialogueText;
+
+    public GameObject flashText;
+
+    private void Start()
+    {
+        save = FindObjectOfType<SaveSerial>();
+
+        dialogue = GetComponent<Dialogue>();
+        dialogueManager = GetComponent<DialogueManager>();
+
+        dialogueText = dialogueTextObject.GetComponent<TextMeshProUGUI>();
+    }
+    void Update()
+    {
+        if (!dialogueBoxObject.activeInHierarchy)
+        {
+            dialogueBoxObject.SetActive(dialogueBoxObject);
+            dialogueTextObject.SetActive(dialogueTextObject);
+            dialogueManager.StartDialogue(dialogue);
+            flashText.SetActive(true);
+            InvokeRepeating("FlashTheText", 0f, 0.5f);
+        }
+        else if (Input.touchCount > 0)
+        {
+            if (dialogueBoxObject.activeInHierarchy && dialogueManager.sentences.Count > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                dialogueManager.DisplayNextSentence();
+            }
+            else if (dialogueBoxObject.activeInHierarchy && dialogueManager.sentences.Count == 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                LoadGameScene();
+            }
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            if (!dialogueBoxObject.activeInHierarchy)
+            {
+                dialogueBoxObject.SetActive(dialogueBoxObject);
+                dialogueTextObject.SetActive(dialogueTextObject);
+                dialogueManager.StartDialogue(dialogue);
+            }
+            else if (dialogueBoxObject.activeInHierarchy && dialogueManager.sentences.Count > 0)
+            {
+                dialogueManager.DisplayNextSentence();
+            }
+            else if (dialogueBoxObject.activeInHierarchy && dialogueManager.sentences.Count == 0)
+            {
+                LoadGameScene();
+            }
+        }
+    }
+
+    void LoadGameScene()
+    {
+        SceneManager.LoadScene("AloeCareGame");
+    }
+
+    void FlashTheText()
+    {
+        if (flashText.activeInHierarchy)
+        {
+            flashText.SetActive(false);
+        }
+        else
+        {
+            flashText.SetActive(true);
+        }
+    }
+}
