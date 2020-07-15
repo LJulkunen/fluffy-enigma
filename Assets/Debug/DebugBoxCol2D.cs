b3ece9460f2d3db443add27ce8c84e7b8140a9f1
Toni <toni.sundell@tuni.fi>
2020-07-15 17:05:44 +0300

    Added box collider debug ONLY IN GAME SCENE

---
+++ Assets/Debug/DebugBoxCol2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DebugBoxCol2D : MonoBehaviour
+{
+    public GameObject debugPrefab;
+    GameObject _debug;
+    BoxCollider2D _col;
+
+    private void Update()
+    {
+        if (!_debug)
+            _debug = Instantiate(debugPrefab, transform);
+
+        if (!_col)
+            _col = GetComponent<BoxCollider2D>();
+
+        _debug.transform.position = transform.position + (Vector3)_col.offset;
+        _debug.transform.localScale = transform.localScale * _col.size;
+    }
+}
